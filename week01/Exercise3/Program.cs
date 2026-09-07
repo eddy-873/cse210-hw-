9a21b6f6db42b0b2820b3d520e3d14136cd844b2
eddy52095 <kamarae873@gmail.com>
2026-09-07 02:59:23 -0900

    asked user for specific number

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("What is your magic number?.");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is you guess number?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+
+            }
+            else
+            {
+                Console.WriteLine("You guessed it");
+            }
+        }
+
     }
 }
